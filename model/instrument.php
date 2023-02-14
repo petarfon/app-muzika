fa9b9fa9c9c7ddfb5466ef0649d3f73faf6fb82b
Petar Lukovac <petar.lukovac@fon.bg.ac.rs>
2023-02-14 08:40:33 +0100

    Implementirane CRUD operacije

--- model/instrument.php
+++ model/instrument.php
@@ -12,6 +12,19 @@ class Instrument{
         $this->nazivInstrumenta = $nazivInstrumenta;
     }
 
-    
+    public static function add($instrument, mysqli $conn){
+        $q = "INSERT INTO instrument(naziv) VALUES ('$instrument')";
+        return $conn->query($q);
+    }
+
+    public static function getByName($instrument, mysqli $conn){
+        $q = "SELECT id FROM instrument WHERE naziv='$instrument' LIMIT 1";
+        return $conn->query($q)->fetch_column();
+    }
+
+    public static function getById($instrument_id, mysqli $conn){
+        $q = "SELECT naziv FROM instrument WHERE id=$instrument_id LIMIT 1";
+        return $conn->query($q)->fetch_column();
+    }
 }
 ?>
